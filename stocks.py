2a879c83c1b9179cf51dfc91e40fb625eb829922
Patrick Z <godcode200@hotmail.com>
2024-11-17 10:45:49 +0800

    lb

--- stocks.py
+++ stocks.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import mplfinance.original_flavor as mpf
 
-# pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 # 沪A实时行情数据
@@ -15,8 +14,8 @@ df_all = ak.stock_sh_a_spot_em()
 # 涨幅 3-5%
 df_zf = df_all.loc[(df_all['涨跌幅'] >= 3) & (df_all['涨跌幅'] <= 5)]
 
-# 量比大于1
-df_lb = df_zf.loc[(df_zf['量比'] > 1)]
+# 量比大于1.2
+df_lb = df_zf.loc[(df_zf['量比'] > 1.2)]
 
 # 换手率 5-10%
 df_hsl = df_lb.loc[(df_lb['换手率'] >= 5) & (df_lb['换手率'] <= 10)]
@@ -88,61 +87,58 @@ for ind in df_final.index:
     kLine_30d(df_final['代码'][ind], df_final['名称'][ind])
 
 
-
-
-
-
-
+############################################################################################################
+############################################################################################################
 # K line - intraday
-def kLine_intraday(code, securityName):
-    now = datetime.datetime.now().replace(microsecond=0)
-    today = now.date
+# def kLine_intraday(code, securityName):
+#     now = datetime.datetime.now().replace(microsecond=0)
+#     today = now.date
 
-    df = ak.stock_zh_a_hist_min_em(symbol=code, start_date=f'{today} 09:30:00', end_date=f'{now}', period='5', adjust="qfq")
-    print(df)
+#     df = ak.stock_zh_a_hist_min_em(symbol=code, start_date=f'{today} 09:30:00', end_date=f'{now}', period='5', adjust="qfq")
+#     print(df)
 
     
-    df2 = df.dropna(how='any').reset_index(drop=True)  # 去除空值且从零开始编号索引
-    df2 = df2.sort_values(by='时间', ascending=True)
+#     df2 = df.dropna(how='any').reset_index(drop=True)  # 去除空值且从零开始编号索引
+#     df2 = df2.sort_values(by='时间', ascending=True)
 
-    # 均线数据
-    df2['5'] = df2["收盘"].rolling(5).mean()
-    df2['10'] = df2["收盘"].rolling(10).mean()
-    df2['20'] = df2["收盘"].rolling(20).mean()
+#     # 均线数据
+#     df2['5'] = df2["收盘"].rolling(5).mean()
+#     df2['10'] = df2["收盘"].rolling(10).mean()
+#     df2['20'] = df2["收盘"].rolling(20).mean()
 
-    plt.style.use("ggplot")
-    fig, ax = plt.subplots(1, 1, figsize=(8, 3), dpi=200)
-    # 绘制 K线
-    mpf.candlestick2_ohlc(ax,
-                      opens=df2['开盘'].values,
-                      highs=df2['最高'].values,
-                      lows=df2['最低'].values,
-                      closes=df2['收盘'].values,
-                      width=0.75, colorup="r", colordown="g")
+#     plt.style.use("ggplot")
+#     fig, ax = plt.subplots(1, 1, figsize=(8, 3), dpi=200)
+#     # 绘制 K线
+#     mpf.candlestick2_ohlc(ax,
+#                       opens=df2['开盘'].values,
+#                       highs=df2['最高'].values,
+#                       lows=df2['最低'].values,
+#                       closes=df2['收盘'].values,
+#                       width=0.75, colorup="r", colordown="g")
 
-    # 显示最高点和最低点
-    ax.text(df2["最高"].idxmax(), df2["最高"].max(), s=df2["最高"].max(), fontsize=8)
-    ax.text(df2["最高"].idxmin(), df2["最高"].min() - 2, s=df2["最高"].min(), fontsize=8)
-    # 显示中文
-    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+#     # 显示最高点和最低点
+#     ax.text(df2["最高"].idxmax(), df2["最高"].max(), s=df2["最高"].max(), fontsize=8)
+#     ax.text(df2["最高"].idxmin(), df2["最高"].min() - 2, s=df2["最高"].min(), fontsize=8)
+#     # 显示中文
+#     plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
-    ax.set_facecolor("white")
-    ax.set_title(securityName)
+#     ax.set_facecolor("white")
+#     ax.set_title(securityName)
 
-    # 画均线
-    plt.plot(df2['5'].values, alpha=0.5, label='MA5')
-    plt.plot(df2['10'].values, alpha=0.5, label='MA10')
-    plt.plot(df2['20'].values, alpha=0.5, label='MA20')
+#     # 画均线
+#     plt.plot(df2['5'].values, alpha=0.5, label='MA5')
+#     plt.plot(df2['10'].values, alpha=0.5, label='MA10')
+#     plt.plot(df2['20'].values, alpha=0.5, label='MA20')
 
-    ax.legend(facecolor='white', edgecolor='white', fontsize=6)
-    # 修改x轴坐标
-    plt.xticks(ticks=np.arange(0, len(df2)), labels=df2["时间"])
-    plt.xticks(rotation=90, size=8)
-    # 修改y轴坐标
-    ax.yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.2f'))
-    # x轴坐标显示不全，整理
-    plt.subplots_adjust(bottom=0.25)
-    plt.show()
+#     ax.legend(facecolor='white', edgecolor='white', fontsize=6)
+#     # 修改x轴坐标
+#     plt.xticks(ticks=np.arange(0, len(df2)), labels=df2["时间"])
+#     plt.xticks(rotation=90, size=8)
+#     # 修改y轴坐标
+#     ax.yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.2f'))
+#     # x轴坐标显示不全，整理
+#     plt.subplots_adjust(bottom=0.25)
+#     plt.show()
 
 # for ind in df_final.index:
 #     kLine_intraday(df_final['代码'][ind], df_final['名称'][ind])
